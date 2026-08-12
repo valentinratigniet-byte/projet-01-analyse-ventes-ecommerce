83201c5819ad8c6d6da1cb95da03e53a9983cd0d
Valentin Ratigniet <valentinratigniet@gmail.com>
2026-08-12 13:40:04 +0200

    feat: colonne state_name (nom complet des états brésiliens)

--- sql/04_star_powerbi.sql
+++ sql/04_star_powerbi.sql
@@ -21,8 +21,22 @@ CREATE OR REPLACE VIEW olist.bi_dim_product AS
 SELECT DISTINCT product_id AS product_key, category
 FROM olist.v_products;
 
+-- state = code (SP) ; state_name = nom complet (São Paulo) pour la carte/lisibilité.
 CREATE OR REPLACE VIEW olist.bi_dim_customer AS
-SELECT customer_id AS customer_key, customer_unique_id, state, city
+SELECT customer_id AS customer_key, customer_unique_id, state, city,
+  CASE state
+    WHEN 'AC' THEN 'Acre' WHEN 'AL' THEN 'Alagoas' WHEN 'AM' THEN 'Amazonas'
+    WHEN 'AP' THEN 'Amapá' WHEN 'BA' THEN 'Bahia' WHEN 'CE' THEN 'Ceará'
+    WHEN 'DF' THEN 'Distrito Federal' WHEN 'ES' THEN 'Espírito Santo'
+    WHEN 'GO' THEN 'Goiás' WHEN 'MA' THEN 'Maranhão' WHEN 'MG' THEN 'Minas Gerais'
+    WHEN 'MS' THEN 'Mato Grosso do Sul' WHEN 'MT' THEN 'Mato Grosso'
+    WHEN 'PA' THEN 'Pará' WHEN 'PB' THEN 'Paraíba' WHEN 'PE' THEN 'Pernambuco'
+    WHEN 'PI' THEN 'Piauí' WHEN 'PR' THEN 'Paraná' WHEN 'RJ' THEN 'Rio de Janeiro'
+    WHEN 'RN' THEN 'Rio Grande do Norte' WHEN 'RO' THEN 'Rondônia'
+    WHEN 'RR' THEN 'Roraima' WHEN 'RS' THEN 'Rio Grande do Sul'
+    WHEN 'SC' THEN 'Santa Catarina' WHEN 'SE' THEN 'Sergipe'
+    WHEN 'SP' THEN 'São Paulo' WHEN 'TO' THEN 'Tocantins'
+    ELSE state END AS state_name
 FROM olist.v_customers;
 
 CREATE OR REPLACE VIEW olist.bi_dim_date AS
